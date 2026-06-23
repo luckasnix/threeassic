cbc231369dc1810e70f6e16b29165e0137e635a8
Kasnix <luc.kasnix@gmail.com>
2026-06-23 09:50:46 -0300

    chore: migrate from package.json to deno.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import deno from "@deno/vite-plugin";
 import { defineConfig } from "vite";
 
 const viteConfig = defineConfig({
+  plugins: [deno()],
   server: {
+    port: 3000,
+  },
+  preview: {
     port: 3000,
   },
 });
